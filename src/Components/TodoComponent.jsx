e6681a8c84a301adcab131b228222a6e3e32ccfc
ABCSDFWLO <pksasdf@gmail.com>
2021-06-05 13:36:33 +0900

    todo아직안완료

--- src/Components/TodoComponent.jsx
+++ src/Components/TodoComponent.jsx
@@ -1,10 +1,34 @@
 import MainLayout from '../Layout/MainLayout';
 import Todo from '../Presenters/Todo';
 
+import {useEffect, useState, usestate} from 'react';
+
 const TodoComponent = () => {
+    const [lists,setlists] = useState(['스터디','공부하기']);
+
+    const addlist = (item) => {
+        setlists(lists.concat(item));
+    }
+    const savelist=()=>{
+        localStorage.setItem('todo',JSON.stringify(lists));
+    }
+    const getlist=()=>{
+        setlists(JSON.parse(localStorage.getItem('todo')));
+    }
+    const removeListItem=(deletIndex)=>{
+        setlists(lists.filter((list,index)=>
+            index!==deletIndex
+        ));
+    }
+    useEffect(()=>{
+        getlist();
+    },[]);
+    useEffect(()=>{
+        savelist();
+    },[lists]);
     return(
         <MainLayout>
-            <Todo></Todo>
+            <Todo lists={lists} addlist={addlist} removeListItem></Todo>
         </MainLayout>
         
     );
--- src/Presenters/Todo.jsx
+++ src/Presenters/Todo.jsx
@@ -1,7 +1,35 @@
-const Todo = () => {
+
+
+const Todo = (props) => {
+
+    const changeInput = (event) => {
+        if (event.key==='Enter'){
+            props.addlist(event.target.value);
+            event.target.value='';
+        }
+    }
+    const askRemoveItem=(event)=>{
+        //물어보기 확인-true 취소-false
+        if(window.confirm('삭제하시겠습니까?')){
+            console.log(event.target.dataset.id);
+            //js에선 배열[ㅁㄴㅇㄹ][ㅇㅇ]를 배열.ㅁㄴㅇㄹ.ㅇㅇ식으로 쓴다함(빠르대)
+            props.removeListItem(parseInt(event.target.dataset.id));
+        }
+    }
+
     return (
         <div>
-            todo
+            <input type='text' onKeyPress={changeInput}/>
+            <div>
+
+                {
+                    props.lists.map((list,index)=>{
+                        return(
+                            <div onClick={askRemoveItem} data-id={index}>{list}</div>
+                        );
+                    })
+                }
+            </div>
         </div>
     );
 }
